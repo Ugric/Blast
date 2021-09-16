4d5610213797840bb5ee100d508de7b5f9b57ca9
Ugric <epicugric@gmail.com>
2021-09-16 21:57:02 +0100

    fix

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="Blast",
+    name="BlastPY",
     version="1.0.0",
     author="W̷̛͒i̶̓͗l̷͗̌ľ̶͘",
     author_email="epicugric@gmail.com",
